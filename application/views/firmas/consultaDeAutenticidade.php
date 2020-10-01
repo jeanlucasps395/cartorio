c16bf1d0c0e64fb627f3256581cf8dea71a56227
CristianR10 <cristian_hiei@hotmail.com>
2020-09-30 22:58:10 -0300

    Consulta de Autenticidade e Consulta de Cartão

--- application/views/firmas/consultaDeAutenticidade.php
+++ application/views/firmas/consultaDeAutenticidade.php
@@ -23,43 +23,79 @@
 				</strong>
 			</div>
 		</div>
-		<div class="consulta__form d-flex">
-			<aside>
-				<div class="consilta__list">
+		<div class="consulta__block">
+			<aside class="col-lg-3">
+				<div class="consulta__block--list">
 					<p>
-						Consulta Autenticidade
+						<i class="fas fa-file"></i><span>Consulta Autenticidade</span>
 					</p>
 					<p>
-						Consulta cartão
+						<i class="fas fa-file"></i><span>Consulta cartão</span>
 					</p>
 				</div>
 			</aside>
-			<form action="">
-				<label for="">
-					Código de segurança N°.:*
-				</label>
-				<input type="text" placeholder="Código de Segurança" required>
-				<br>
-				<label for="">
-					Código de segurança.:
-				</label>
-				<span class="btn-p">638508</span>
-				<br>
-				<label for="">
-					Re-digite.:*
-				</label>
-				<input type="text" placeholder="Código" required>
-				<label for="">
-					ip.:
-				</label>
-				<span>117.97.145.118</span>
-				<div class="col-6">
+			<ul class="consulta__search col-lg-9">
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">Código de segurança Nº.:<span>*</span></strong>
+					<input class="col-6" type="text" name="" id="" placeholder="Código de Segurança">
+				</li>
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">Código de segurança</strong>
+					<button class="btn-p col-6 col-lg-25">687316</button>
+				</li>
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">Código</strong>
+					<input class="col-6" type="text" name="" id="" placeholder="Código">
+				</li>
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">IP.:</strong>
+					<span class="col-6">177.97.145.118</span>
+				</li>
+				<li class="consulta__search--btn d-flex">
 					<button class="btn-p">Consultar</button>
-				</div>
-				<div class="col-6">
-					<button class="btn-s">Limpar</button>
-				</div>
-			</form>
+					<button class="btn-w">Limpar</button>
+				</li>
+			</ul>
+			<ul class="consulta__card col-lg-9">
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">Escolha o documento.:<span>*</span></strong>
+					<ul>
+						<li>
+							<input class="col-1" type="radio" name="" id="">
+							<strong>NOME</strong>
+						</li>
+						<li>
+							<input class="col-1" type="radio" name="" id="">
+							<strong>CPF</strong>
+						</li>
+						<li>
+							<input class="col-1" type="radio" name="" id="">
+							<strong>RG</strong>
+						</li>
+					</ul>
+				</li>
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">Nome.:<span>*</span></strong>
+					<input class="col-6" type="text" name="" id="" placeholder="Documento">
+				</li>
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">Código de segurança</strong>
+					<button class="btn-p col-6 col-lg-2">687316</button>
+				</li>
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">Re-digite</strong>
+					<input class="col-6" type="text" name="" id="" placeholder="Código">
+				</li>
+				<li class="d-flex">
+					<strong class="col-6 col-lg-3">IP.:</strong>
+					<span class="col-6">177.97.145.118</span>
+				</li>
+				<li class="consulta__search--btn d-flex">
+					<button class="btn-p">Consultar</button>
+					<button class="btn-w">Limpar</button>
+				</li>
+			</ul>
 		</div>
+
 	</div>
 </div>
